974d54d11e16f98ca3bdee4174409edea337d53f
Thomasvdv31 <thomas.vdv@hotmail.com>
2024-07-09 07:21:59 +0200

    - Added Edit and delete features - Added different validations client side and server side - Added bootstrap theme - Added toastr - Added razor project for learning purposes.

--- BulkyWebRazor_Temp/Models/Category.cs
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
-namespace BulkyWeb.Models
+namespace BulkyWebRazor_Temp.Models
 {
     public class Category
     {
@@ -12,7 +12,7 @@ namespace BulkyWeb.Models
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage = "Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
