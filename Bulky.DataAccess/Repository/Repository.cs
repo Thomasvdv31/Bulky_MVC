7d867873b3e04972f30eca2858c1f46a25f08ffa
Thomasvdv31 <thomas.vdv@hotmail.com>
2024-07-10 07:32:29 +0200

    - Reconfigured files using N-Tier Architecture - Added a Repository pattern - Finished Razor project for learning purposes. - Added Areas

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.Irepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+            // EXAMPLE: _db.Categories == dbSet
+        }
+
+        public void Add(T entity)
+        {
+            
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            // EXAMPLE:  RETURN = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
